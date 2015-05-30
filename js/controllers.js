4c4f383e38caeaa74a28c8dcac192598e5beeb79
respectYourNeighbour <philip_luca@yahoo.com>
2015-05-30 13:11:03 +0300

    clean controller
    clean controller

--- js/controllers.js
+++ js/controllers.js
@@ -4,34 +4,12 @@
 
 var myAngularApp = angular.module('myAngularApp', []);
 
-// +++++++++++++++++++++++++++++++++++++++
-// 'Data' without a $http request;       +
-// You don't need a web-server for this; +
-// +++++++++++++++++++++++++++++++++++++++
-/*
-myAngularApp.controller('myListCtrl', function($scope) {
-  $scope.heroes = [
-    {'name': 'Spider-Man',
-     'snippet': 'Fast and it has the ability to create spider-webs.',
-     'age':'21'},
-    {'name': 'Storm',
-     'snippet': 'Has the ability to start weather storms.',
- 	 'age':'32'},
-    {'name': 'Profesor-X™',
-     'snippet': 'Super chill cool guy.',
-     'age':'43'}
-  ];
-*/
-
-
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // 'Data' fetched with a $http request;                    +
 //  You need a web-server for this up and running;         +
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 myAngularApp.controller('myListCtrl', ['$scope', '$http', function($scope, $http) {
-	$http.get('resources/lista.json' , {cache:true}).success(function(data) {
-		$scope.heroes = data;
-	});
 
 	$scope.name = 'user';
 	$scope.orderProperty = 'age';
